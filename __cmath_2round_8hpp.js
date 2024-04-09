961312f8cfcbec31e02e3b0202ec99c4526facf4
tobanteEmbedded <tobanteEmbedded@gmail.com>
2024-04-09 19:03:38 +0000

    Update from https://github.com/tobanteEmbedded/tetl/commit/f25bb107279206fd1b6b1d2e98a1fc4d821f78c3

--- __cmath_2round_8hpp.js
+++ __cmath_2round_8hpp.js
@@ -2,8 +2,8 @@ var __cmath_2round_8hpp =
 [
     [ "round", "__cmath_2round_8hpp.html#gad20c80803dd9917b164ebef227db3ed1", null ],
     [ "round", "__cmath_2round_8hpp.html#ga8ece6b0eeb98b1650638dbbbb09cda23", null ],
+    [ "round", "__cmath_2round_8hpp.html#ga594cdef05f79d80bbcb2c5f4b599b52e", null ],
     [ "round", "__cmath_2round_8hpp.html#ga3cc8394cdaabbc0790662af74f6574a3", null ],
-    [ "round", "__cmath_2round_8hpp.html#ga35ecfb26da0d8c15ff0f23281b6d4fa5", null ],
     [ "roundf", "__cmath_2round_8hpp.html#ga25f67bbe153f9daa0cbd56587131bfd3", null ],
     [ "roundl", "__cmath_2round_8hpp.html#gae94747d1de39c6586935e11628963eee", null ]
 ];
--- exp_8hpp.js
+++ exp_8hpp.js
@@ -1,9 +1,9 @@
 var exp_8hpp =
 [
-    [ "exp", "exp_8hpp.html#ga661cc4fffe4d999611477bbbfcc7e436", null ],
-    [ "exp", "exp_8hpp.html#ga6f9635830aa0cf956dc1924dbd841ad8", null ],
-    [ "exp", "exp_8hpp.html#ga32583e05b36ffdddc54f1ca3ec70a5b3", null ],
-    [ "exp", "exp_8hpp.html#gabfd62b20f90b6b5275854d4bd14bb89a", null ],
-    [ "expf", "exp_8hpp.html#gabc4969b436b3ff78af213086a1d5fe52", null ],
-    [ "expl", "exp_8hpp.html#ga5fb6fdb732877131d7d8eaa6bf8fde15", null ]
+    [ "exp", "exp_8hpp.html#ga744fd84929a0ba83626367fb0814fcc4", null ],
+    [ "exp", "exp_8hpp.html#gadcf2fa322adfe3a7e284b01811eaec85", null ],
+    [ "exp", "exp_8hpp.html#gaf4de36755d387716338b5639cf6202fb", null ],
+    [ "exp", "exp_8hpp.html#ga93fc39900b41ce0c27c840f4864eb7e7", null ],
+    [ "expf", "exp_8hpp.html#ga0968afe0e7ec491a982e2f849100400d", null ],
+    [ "expl", "exp_8hpp.html#gab87b218f1fc55b6fc30ccc076b6576a1", null ]
 ];
